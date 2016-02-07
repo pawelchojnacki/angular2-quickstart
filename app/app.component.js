74812b9cb2542c0397b4a1041daa0eace140717c
pawelchojnacki <chojnacki.pawel@linux.com>
2016-02-07 17:45:19 +0100

    hero interface

--- app/app.component.js
+++ app/app.component.js
@@ -18,13 +18,15 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = 'Tour of Heroes';
-                    this.hero = 'Windstorm';
+                    this.hero = {
+                        id: 1,
+                        name: 'Windstorm'
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+                        template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,17 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 
 export class AppComponent {
-  public title = 'Tour of Heroes';
-  public hero = 'Windstorm';
+  public hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+  };
+}
+
+interface Hero {
+    id: number;
+    name: string;
 }
