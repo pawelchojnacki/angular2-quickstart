775798d4d3b009195844b010ff88e1d3c57582c3
pawelchojnacki <chojnacki.pawel@linux.com>
2016-02-07 17:53:42 +0100

    input form working correctly

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,11 +6,15 @@ import {Component} from 'angular2/core';
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name:</label>{{hero.name}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    </div>
     `
 })
 
 export class AppComponent {
+    public title: 'Tour of Heroes';
   public hero: Hero = {
       id: 1,
       name: 'Windstorm'
